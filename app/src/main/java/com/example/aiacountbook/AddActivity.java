0e914baec0e2e8b09070fdd7d4e9d3c03328732d
LeeYelim <65666914+LeeYealim@users.noreply.github.com>
2022-05-03 03:12:02 +0900

    영수증 추가 화면

--- app/src/main/java/com/example/aiacountbook/AddActivity.java
+++ app/src/main/java/com/example/aiacountbook/AddActivity.java
@@ -2,13 +2,43 @@ package com.example.aiacountbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 public class AddActivity extends AppCompatActivity {
+
+    private TextView edit_date;
+    private DatePickerDialog.OnDateSetListener callbackMethod;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+    }
+
+    public void InitializeView()
+    {
+        edit_date = (TextView) findViewById(R.id.edit_date);
+    }
+
+    public void InitializeListener()
+    {
+        callbackMethod = new DatePickerDialog.OnDateSetListener()
+        {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth)
+            {
+                edit_date.setText(year + "년" + monthOfYear + "월" + dayOfMonth + "일");
+            }
+        };
+    }
+
+    public void OnClickHandler(View view)
+    {
+        DatePickerDialog dialog = new DatePickerDialog(this, callbackMethod, 2019, 5, 24);
+        dialog.show();
     }
 }
