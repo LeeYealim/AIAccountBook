9b3e3ac649495cadf6794e5409eebb0f86041f8d
LeeYealim <1999n4w16i@gmail.com>
2022-05-19 02:11:05 +0900

    등록 화면 닫기 버튼 기능

--- app/src/main/java/com/example/aiacountbook/AddActivity.java
+++ app/src/main/java/com/example/aiacountbook/AddActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
+import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -15,6 +16,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -49,12 +51,40 @@ public class AddActivity extends AppCompatActivity {
         ab.setDisplayHomeAsUpEnabled(true);     // 위로 버튼 활성화
         ab.setTitle("영수증 등록");               // 앱바 타이틀 설정
 
+        // 취소 버튼 클릭 시 화면 종료
+        Button btn = findViewById(R.id.btn_close);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
+
+        // 날짜 선택 버튼
+        ImageButton btn_date = (ImageButton)findViewById(R.id.btn_date);
+        btn_date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("yelim","date 버튼 클릭 이벤트");
+                callbackMethod = new DatePickerDialog.OnDateSetListener()
+                {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth)
+                    {
+                        TextView textView_Date = (TextView)findViewById(R.id.textview_date);
+                        textView_Date.setText(String.format("%d-%d-%d ", year ,monthOfYear+1,dayOfMonth));
+                    }
+                };
+
+                DatePickerDialog dialog = new DatePickerDialog(AddActivity.this, callbackMethod, 2020, 12, 0);
+                dialog.show();
+            }
+        });
+
+        
         btnCamera = (Button) findViewById(R.id.btn_camera);
         imageView = (ImageView) findViewById(R.id.image_view);
-        //btnCamera.setOnClickListener(this);
-
-
         btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -69,6 +99,11 @@ public class AddActivity extends AppCompatActivity {
         });
 
     }
+    
+    
+    
+    
+    
 
     private String currentDateFormat() {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HH_mm_ss");
@@ -133,72 +168,5 @@ public class AddActivity extends AppCompatActivity {
             } else
                 Toast.makeText(getApplicationContext(), "mPhotoFile is null", Toast.LENGTH_SHORT).show();
         }
-
     }
-
-
-
-
-
-
-
-
-
-
-
-
-//    // 사진 촬영 후 돌아와서 이미지뷰에 표시
-//    @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);  // 카메라 촬영을 하면 이미지뷰에 사진 삽입
-//        if(requestCode == 0 && resultCode == RESULT_OK) {       // Bundle로 데이터를 입력
-//            Bundle extras = data.getExtras();                   // Bitmap으로 컨버전
-//            Bitmap imageBitmap = (Bitmap) extras.get("data");   // 이미지뷰에 Bitmap으로 이미지를 입력
-//            imageView.setImageBitmap(imageBitmap);
-//        }
-//
-////        if(requestCode == 0 && resultCode == RESULT_OK) { // 이미지뷰에 파일경로의 사진을 가져와 출력
-////            imageView.setImageURI(photoUri);
-////        }
-//
-//    }
-//    // ImageFile의 경로를 가져올 메서드 선언
-//    private File createImageFile() throws IOException { // 파일이름을 세팅 및 저장경로 세팅
-//        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-//        String imageFileName = "JPEG_" + timeStamp + "_";
-//        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-//        File StorageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-//        File image = File.createTempFile( imageFileName, ".jpg", storageDir );
-//        return image;
-//    }
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//    public void InitializeView()
-//    {
-//        edit_date = (TextView) findViewById(R.id.edit_date);
-//    }
-//
-//    public void InitializeListener()
-//    {
-//        callbackMethod = new DatePickerDialog.OnDateSetListener()
-//        {
-//            @Override
-//            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth)
-//            {
-//                edit_date.setText(year + "년" + monthOfYear + "월" + dayOfMonth + "일");
-//            }
-//        };
-//    }
-//
-//    public void OnClickHandler(View view)
-//    {
-//        DatePickerDialog dialog = new DatePickerDialog(this, callbackMethod, 2019, 5, 24);
-//        dialog.show();
-//    }
 }
